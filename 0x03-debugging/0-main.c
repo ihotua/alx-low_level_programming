3d3fa31af4171470134cdf39ef30503dd84fbb72
Ihotu Alaku <ihotuagbo@gmail.com>
2023-02-23 05:35:46 -0800

    Debugging task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,18 @@
 #inlcude "main.h"
 
 /**
- * main - Testfunction for positive or negatve
+ * main - Testfunction for positive or negative 
  * Return: 0
  */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
+	i = 0;
+	positive_or_negative(i);
+	
 	return (0);
 }
+
+
