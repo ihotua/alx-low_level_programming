9c4171b04581da56c78ec1ce9000df6b9d71cb58
Ihotu Alaku <ihotuagbo@gmail.com>
2023-03-02 04:55:26 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p)
+	{
+		p++;
+	}
+
+	while (*src)
+	{
+		*p++ = *src++;
+	}
+
+	*p = '\0';
+
+	return (dest);
+}
