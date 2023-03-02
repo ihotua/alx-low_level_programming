b65baa4773e12c7230b1a89128f1112bcef8bc85
Ihotu Alaku <ihotuagbo@gmail.com>
2023-03-02 13:54:17 -0800

    CAa string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,16 +6,33 @@
  * Return: A pointer to the changed string.
  */
 
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
-	int i = 0;
-	while (str[i] != '\0')
+	int index = 0;
+
+	while (str[index])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 'a' + 'A';
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	i++;
-	}
+
 	return (str);
 }
