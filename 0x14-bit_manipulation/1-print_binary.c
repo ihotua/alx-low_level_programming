64f4a660c2dac0a6264706531d0847d511ae7fbc
root <ihotuagbo@gmail.com>
2024-02-18 14:43:43 -0800

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,25 @@
 #include <stdio.h>
 
 /**
- * print_binary - prints a number in binary rotation
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int result = 1;
+	for (; power > 0; power--)
+	{
+		result *= base;
+	}
+	return (result);
+}
+
+/**
+ * print_binary - prints a number in binary notation
  * @n: number to print
  *
  * Return: void
@@ -10,13 +28,19 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	unsigned long int divisor;
 
-	while (mask > 0)
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0) 
 	{
-		_putchar((n & mask) ? '1' : '0');
-		mask >>= 1;
+		if (n & divisor)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
 	}
-
-	_putchar('\n');
 }
