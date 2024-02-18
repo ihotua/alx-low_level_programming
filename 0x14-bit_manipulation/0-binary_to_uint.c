aa1cea50673ceca09481e2d06d0dddb22e8ceade
root <ihotuagbo@gmail.com>
2024-02-18 11:20:36 -0800

    Binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	return (0);
 
 	while (b[c] != '\0')
-        c++;
+		c++;
 	
 	c--;
 
@@ -26,7 +26,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[c] != '0' && b[c] != '1')
 		return (0);
-        	if (b[c] == '1')
+		if (b[c] == '1')
 		{
 			i += k;
 		}
