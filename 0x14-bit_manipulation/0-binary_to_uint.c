ea237443f4420ae90915f3f4c57de8318d8406b8
root <ihotuagbo@gmail.com>
2024-02-18 10:56:32 -0800

    Binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,27 +10,27 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int i = 0;
-    unsigned int k = 1;
-    int c = 0; 
+	unsigned int i = 0;
+	unsigned int k = 1;
+	int c = 0;
 
-    if (b == NULL)
-        return 0;
+	if (b == NULL)
+	return (0);
 
-    while (b[c] != '\0')
+	while (b[c] != '\0')
         c++;
+	
+	c--;
 
-    c--;
-
-    for (; c >= 0; c--)
-    {
-        if (b[c] != '0' && b[c] != '1')
-            return 0;
-        if (b[c] == '1')
-        {
-            i += k;
-        }
-        k *= 2;
-    }
-    return i;
+	for (; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+		return (0);
+        	if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
+	}
+	return i;
 }
