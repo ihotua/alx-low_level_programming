f491743ee6f758d75d4b7b68d8fecbe2787fd876
root <ihotuagbo@gmail.com>
2024-02-27 18:33:23 -0800

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,14 +9,11 @@
 
 void print_binary(unsigned long int n)
 {
-    if (n >= 2)
-    {
-        if (n >> 1)
-            print_binary(n >> 1);
-        _putchar((n & 1) + '0');
-    }
-    else
-    {
-        _putchar('0');
-    }
+  unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+
+	while (mask > 0)
+	{
+		_putchar((n & mask) ? '1' : '0');
+		mask >>= 1;
+	}
 }
