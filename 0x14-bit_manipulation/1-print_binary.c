c8bd7924c0f0f7eca241b3ae508297de159e9953
root <ihotuagbo@gmail.com>
2024-02-27 18:04:12 -0800

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,6 @@
  *
  * Return: value of (base ^ power)
  */
-
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
 	unsigned long int num;
@@ -20,23 +19,37 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 }
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: unsigned long int
+ * print_binary - prints a number in binary notation
+ * @n: number to print
  *
- * Return: no return
+ * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int divisor;
+	unsigned long int divisor, mask;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
 	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
 	while (divisor != 0)
 	{
-		if (n & divisor)
+		mask = n & divisor;
+		if (mask != 0)
 			_putchar('1');
 		else
 			_putchar('0');
 		divisor >>= 1;
 	}
+}
+
+int main()
+{
+	unsigned long int num = 1;
+	print_binary(num);
+	printf("\n");
+	return (0);
 }
