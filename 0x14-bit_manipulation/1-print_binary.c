2033a18fe98e6d13bec98bfa753ff019e0c03e43
root <ihotuagbo@gmail.com>
2024-02-27 18:13:19 -0800

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,5 @@
 #include "main.h"
-
-/**
- * _pow - func calculates (base ^ power)
- * @base: base of the exponent
- * @power: power of the exponent
- *
- * Return: value of (base ^ power)
- */
-unsigned long int _pow(unsigned int base, unsigned int power)
-{
-	unsigned long int num;
-	unsigned int a;
-
-	num = 1;
-	for (a = 1; a <= power; a++)
-		num *= base;
-	return (num);
-}
+#include <stdio.h>
 
 /**
  * print_binary - prints a number in binary notation
@@ -24,32 +7,35 @@ unsigned long int _pow(unsigned int base, unsigned int power)
  *
  * Return: void
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int divisor, mask;
+	unsigned long int divisor, check;
+	char flag;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	flag = 0;
+	divisor = 1 << (sizeof(unsigned long int) * 8 - 1);
 	while (divisor != 0)
 	{
-		mask = n & divisor;
-		if (mask != 0)
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
 			_putchar('1');
-		else
+		}
+		else if (flag == 1 || divisor == 1)
+		{
 			_putchar('0');
+		}
 		divisor >>= 1;
 	}
 }
 
 int main()
 {
-	unsigned long int num = 1;
-	print_binary(num);
-	printf("\n");
+	print_binary(0);
+	_putchar('\n');
+	print_binary(1);
+	_putchar('\n');
 	return (0);
 }
