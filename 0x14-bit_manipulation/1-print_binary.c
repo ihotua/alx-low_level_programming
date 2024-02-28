01f13d83e518e309fe6b1f936aa3d9f78852970f
root <ihotuagbo@gmail.com>
2024-02-27 18:26:54 -0800

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,41 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_binary - prints a number in binary notation
- * @n: number to print
+ * print_binary - prints the binary representation of a number
+ * @n: unsigned long int.
  *
- * Return: void
+ * Return: no return.
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int divisor, check;
-	char flag;
-
-	flag = 0;
-	divisor = 1 << (sizeof(unsigned long int) * 8 - 1);
-	while (divisor != 0)
-	{
-		check = n & divisor;
-		if (check == divisor)
-		{
-			flag = 1;
-			_putchar('1');
-		}
-		else if (flag == 1 || divisor == 1)
-		{
-			_putchar('0');
-		}
-		divisor >>= 1;
-	}
-}
-
-int main()
-{
-	print_binary(0);
-	_putchar('\n');
-	print_binary(1);
-	_putchar('\n');
-	return (0);
+    if (n >= 2)
+    {
+        if (n >> 1)
+            print_binary(n >> 1);
+        _putchar((n & 1) + '0');
+    }
+    else
+    {
+        _putchar('0');
+    }
 }
