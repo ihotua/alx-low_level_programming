20e0486c38f11915dc1abeb8e4f7140b709cadfa
root <ihotuagbo@gmail.com>
2024-02-29 12:41:16 -0800

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,12 +10,13 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 0)
+	if (n >> 0)
 	{
+		if (n >> 1)	
 		print_binary(n >> 1);
 		_putchar('0' + (n & 1));
 	}
-	else 
+	else
 	{
 		_putchar('0');
 	}
