b387c0ee36a284f18f10abff9d0e8dc77d94ea2e
root <ihotuagbo@gmail.com>
2024-02-29 14:06:30 -0800

    Get Bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,17 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask = 1;
+	
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
-	unsigned longint mask = 1 << index;
-	return (mask & n) != 0;
+	mask <<= index;
+
+	if (mask & n)
+		return (1);
+	else
+		return (0);
 }
