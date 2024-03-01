03044e7c78efb4940450988d5457962e14ec4a3a
root <ihotuagbo@gmail.com>
2024-03-01 05:54:28 -0800

    Clear Bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,9 +12,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask = 1UL << index;
+	unsigned long int mask = 1 << index;
 
-	if (index < sizeof(unsigned long int) * 8 || n == NULL)
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
 	{
 		*n = (*n & ~mask);
 		return (1);
