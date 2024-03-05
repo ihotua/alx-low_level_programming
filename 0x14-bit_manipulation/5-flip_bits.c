ec7d04c5b475dfa55048293f3132d21cbf5d2e6c
root <ihotuagbo@gmail.com>
2024-03-05 12:03:02 -0800

    Flip Bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - Number of bits needed to flip to get from one number to another
+ *
+ * @n: long int
+ *
+ * @m: unsigned int
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned long int sabi = n ^ m; 
+
+	while (sabi != 0)
+	{
+		count += (sabi & 1);
+		sabi >>= 1;
+	}
+
+	return (count);
+
+}
