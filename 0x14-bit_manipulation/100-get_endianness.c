344a3c0e0bfdc61bb0f0f05b86af3f5ec828023c
root <ihotuagbo@gmail.com>
2024-03-05 12:45:35 -0800

    Get Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 1 if little endian, 0 if big endian
+ */
+
+int get_endianness(void)
+{
+	unsigned alex = 1;
+	char *cat = (char *)&alex;
+	return (*cat == 1);
+	
+}
