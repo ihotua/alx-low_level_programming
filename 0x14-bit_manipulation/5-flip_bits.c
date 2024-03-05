14209321d213f4d58e6c47ccddda3818a9a67ea7
root <ihotuagbo@gmail.com>
2024-03-05 12:11:33 -0800

    Flip Bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0;
-	unsigned long int sabi = n ^ m; 
+	unsigned long int sabi = n ^ m;
 
 	while (sabi != 0)
 	{
