ac52a45087f095767b10ea53e180badb09685243
root <ihotuagbo@gmail.com>
2024-04-16 17:30:55 -0700

    File IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -23,22 +21,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
     if (fd == -1)
         return 0;
 
-    struct stat st;
-    if (fstat(fd, &st) == -1) {
-        close(fd);
-        return 0;
-    }
-
-    size_t file_size = st.st_size;
-
-    char *buffer = malloc(file_size);
+    char *buffer = malloc(sizeof(char) * letters);
     if (buffer == NULL)
     {
         close(fd);
         return 0;
     }
 
-    ssize_t bytes_read = read(fd, buffer, file_size);
+    ssize_t bytes_read = read(fd, buffer, letters);
     if (bytes_read == -1)
     {
         free(buffer);
