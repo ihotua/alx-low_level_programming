4e6dfc7a0c7c8acb383490f121d5afff467a0908
root <ihotuagbo@gmail.com>
2024-04-21 16:42:21 -0700

    File IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -41,7 +41,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	bytes_written = fwrite(buffer, sizeof(char), bytes_read,stderr);
+	bytes_written = fwrite(buffer, sizeof(char), bytes_read, stderr);
 	free(buffer);
 	fclose(file);
 
