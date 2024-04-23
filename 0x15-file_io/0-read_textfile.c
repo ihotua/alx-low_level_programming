bf0991ea6bbc04475489ebd3da4c198f8eb16df3
root <ihotuagbo@gmail.com>
2024-04-23 15:16:12 -0700

    File IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
