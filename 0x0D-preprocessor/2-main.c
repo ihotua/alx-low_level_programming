edb761a24ead47b08f09edd3f22040dc68d6017b
root <ihotuagbo@gmail.com>
2024-08-21 04:34:25 -0700

    MainC

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,10 +4,10 @@
  * main - prints the name of the file it was compiled from
  *
  * Return: Always 0 (Success)
- */ 
+ */
 
-int main()
+int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
