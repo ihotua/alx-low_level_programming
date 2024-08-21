2682eb5982c9a9098be89cf9c849b20328d7f561
root <ihotuagbo@gmail.com>
2024-08-21 04:51:02 -0700

    Function Like Macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+/**
+ * File: 3-function_like_macro
+ * @X: input
+ * Desc: Header file that defines a macro to compute the abs value of a number.
+ */
+
+#define ABS(X) ((X) < 0 ? -(X) : (X))
+
+#endif /* MACRO_H */
+
